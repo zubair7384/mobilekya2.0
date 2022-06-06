7183af838ee01b1bb3a774988d5f55448ce74d2b
zubair7384 <zubair.iqbal2018@gmail.com>
2022-06-06 18:29:19 +0400

    bg filter

--- components/homeBanner/HomeBanner.js
+++ components/homeBanner/HomeBanner.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../../context/ThemeContext";
 import Image from "next/image";
 import styles from "./HomeBanner.module.scss";
 
 const HomeBanner = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div className={styles.Container}>
-      <div className={styles.Wrapper}>
-        <div className={styles.BannerImageWrapper}>
-          <Image
-            className={styles.BannerImage}
-            src="/bannerPerson.svg"
-            alt="mobilekya banner person"
-            width={100}
-            height={100}
-          />
+    <div className={theme}>
+      <div
+        className={`${styles.Container} ${
+          theme === "Dark" ? "DarkFilter" : ""
+        }`}
+      >
+        <div className={styles.Wrapper}>
+          <div className={styles.BannerImageWrapper}>
+            <Image
+              className={styles.BannerImage}
+              src="/bannerPerson.svg"
+              alt="mobilekya banner person"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
       </div>
     </div>
